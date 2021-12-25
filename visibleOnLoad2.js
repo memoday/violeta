dc81590d0f2fadb39c5d23c5b2330eefae3c09fb
memoday <74040890+memoday@users.noreply.github.com>
2021-12-25 11:53:42 +0900

    detail fixed

--- visibleOnLoad2.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-    var innerWidth = window.innerWidth;
-
-    innerWidth <= "960" ? document.getElementById('vioChat').style.visibility = "hidden" : document.getElementById('vioChat').style.visibility = "visible";
-  }, true);
